615ad5ba4aa74ce4acc64112b27afd577021f2c2
Aze <gaby.colban@gmail.com>
2025-02-03 13:03:40 +0100

    page pays

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Configuration du moteur de template EJS
+app.set("view engine", "ejs");
+
+// Route pour afficher index.ejs
+app.get("/", (req, res) => {
+    res.render("index"); // Pas besoin d'écrire ".ejs", c'est automatique
 });
 
+// Démarrage du serveur
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
