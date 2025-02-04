9d3376c68235089433d81559e7632c9ad5239f8d
Alexis <alexishazebrouck503@gmail.com>
2025-02-04 16:17:37 +0100

    :bug: Fix require time #15

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const NodeCache = require("node-cache");
+const requireTime = require('require-time')();
 
 dotenv.config();
 
